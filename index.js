37471ffc6ccf524d148b65f85b44d99788f18262
AnzarAli <anzarali150799@gmail.com>
2022-03-21 13:33:50 +0530

    created a readable stream to read an exoplanet csv file

---
+++ index.js
@@ -0,0 +1,26 @@
+const {parse} = require('csv-parse');
+const fs = require('fs');
+const { resourceLimits } = require('worker_threads');
+
+function isHabitablePlanet(planet) {
+    return planet['koi_disposition'] === 'CONFIRMED'
+}
+const habitablePlanets = [];
+fs.createReadStream('kepler_data.csv')
+.pipe(parse({
+    comment:'#',
+    columns:true,
+}))
+.on('data',(data) => {
+    if(isHabitablePlanet(data));
+    habitablePlanets.push(data)
+})
+.on('error',(err) => {
+    console.log(err)
+})
+.on('end',() => {
+    console.log(results);
+    console.log('done');
+})
+
+
